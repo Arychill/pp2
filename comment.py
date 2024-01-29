d9a7cd38b5eb4ee4c95c584cea7b3f0fb8b1752c
Arychill <110170966+Arychill@users.noreply.github.com>
2024-01-30 04:12:40 +0600

    Add files via upload

---
+++ comment.py
@@ -0,0 +1,9 @@
+print("Moldabek Argyn") #it prints my name
+"""
+This is a comment
+written in
+more than just one line
+and the next line will print
+pp2
+"""
+print("programming principles 2")
---
+++ syntax.py
@@ -0,0 +1,11 @@
+x = 5
+y = "Hello, World!"
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+#This is a comment.
+print("Hello, World!")
+print(y)
